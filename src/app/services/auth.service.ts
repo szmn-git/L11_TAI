a48f64eea994b510e30a8f04cfdcd3fc9284fa42
Szymon Kieronski <informatyczne123@gmail.com>
2021-05-28 18:04:32 +0200

    final change

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,7 +10,7 @@ import {Token} from "../models/token";
 })
 export class AuthService {
 
-  private url = 'https://tai21-test.herokuapp.com/api';
+  private url = 'https://l11-tai-deploy.herokuapp.com';
 
   constructor(private http: HttpClient) {
   }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from "@angular/common/http";
 })
 export class DataService {
 
-  private url = 'https://l11-tai-deploy.herokuapp.com/';
+  private url = 'https://l11-tai-deploy.herokuapp.com';
 
   constructor(private http: HttpClient) {
   }
